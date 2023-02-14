192a8ec91277fdbf1f657d38aee71e1ba9aece7a
x-dcx-x <petrisot.guillaume@gmail.com>
2023-02-14 16:04:25 +0100

    commit initial

--- index.php
+++ index.php
@@ -1,35 +1,44 @@
 <?php
 
 // Premiere ligne
-$i = 1;
 
-($i < 6)
-  echo $i;
-  $i++;
+$i = 1;
+do {
+
+    echo $i . "<br>";
+    $i++;
+} while ($i < 6);
 
 //Deuxieme ligne
-echo"<br><br>";
+
+echo "<br><br>";
 
 $i = 1;
 
- {
-     echo $i;
-     $i++;
- }  ($i < 6);
+while ($i < 6) {
+    echo $i;
+    $i++;
+}
 
-echo"<br><br>";
+echo "<br><br>";
+
 //Troisieme ligne
- ($i = 0; $i < 10; ) {
+$i = 0;
+
+for ($i = 0; $i < 9;) {
+    $i++;
+    $x = $i . ' ' . 'eme passage dans la boucle' . "<br>";
     echo $x;
 }
 
+echo "<br><br>";
 
-echo"<br><br>";
 //Quatrieme ligne
+
 $colors = array("red", "green", "blue", "yellow");
 
- ($colors  $x) {
-    echo $x;
+ foreach ($colors as $x) {
+    echo "Dans ce tableau il y a les couleurs $x. <br>";
 }
 
 
